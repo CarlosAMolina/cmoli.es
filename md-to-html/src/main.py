0265fd03794290cdd7df4c98d1b5a38aecae9e33
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-10-19 22:18:10 +0200

    Add DirectoryAnalyzer and replace PurePosixPath with PurePath

--- md-to-html/src/main.py
+++ md-to-html/src/main.py
@@ -1,10 +1,12 @@
+import os
 import pathlib
+import typing as tp
 
 
 class FilenameExtensionDetector:
     @staticmethod
     def is_md(filename: str) -> bool:
-        return pathlib.PurePosixPath(filename).suffix == ".md"
+        return pathlib.PurePath(filename).suffix == ".md"
 
 
 class FilenameWithExtension:
@@ -21,3 +23,20 @@ class FilenameWithExtension:
     @property
     def md(self) -> str:
         return self._get_filename_with_extension(".md")
+
+
+class DirectoryAnalyzer:
+    def get_md_pathnames(self, path: str) -> tp.Iterator[str]:
+        for (dir_pathname, dirnames, filenames) in os.walk(path):
+            print(dir_pathname, dirnames, filenames)
+            for filename in filenames:
+                if FilenameExtensionDetector.is_md(filename):
+                    yield pathlib.PurePath(dir_pathname, filename)
+
+
+if __name__ == "__main__":
+    # TODO move code to e2e test
+    script_dir = pathlib.Path(__file__).parent.absolute()
+    dir_to_analize = pathlib.PurePath(script_dir.parent.parent, "src")
+    for pathname in DirectoryAnalyzer().get_md_pathnames(dir_to_analize):
+        print(pathname)
