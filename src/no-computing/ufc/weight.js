a113555e00dae442d4b30dcb6dee6fdcd81b5dca
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-11-30 22:09:21 +0100

    Refactor ufc weight.js use camel case

--- src/no-computing/ufc/weight.js
+++ src/no-computing/ufc/weight.js
@@ -1,4 +1,4 @@
-const tags_prefix_id_to_convert = [
+const tagsPrefixIdToConvert = [
   "strawweight",
   "flyweight",
   "bantamweight",
@@ -10,25 +10,25 @@ const tags_prefix_id_to_convert = [
   "heavyweight",
 ]
 
-for (const tag_prefix_id of tags_prefix_id_to_convert) {
-  setKg(tag_prefix_id)
+for (const tagPrefixId of tagsPrefixIdToConvert) {
+  setKg(tagPrefixId)
 }
 
-function setKg(tag_prefix_id) {
-  var tag_kg = tag_prefix_id+"-kg";
-  var tag_pounds = tag_prefix_id+"-pounds";
-  var pounds = document.getElementById(tag_pounds).innerHTML;
+function setKg(tagPrefixId) {
+  var tagKg = tagPrefixId+"-kg";
+  var tagPounds = tagPrefixId+"-pounds";
+  var pounds = document.getElementById(tagPounds).innerHTML;
   var kg = getKgFromPounds(pounds);
   kg = getNumberRoundDecimals(kg, 3);
-  document.getElementById(tag_kg).innerHTML = kg;
+  document.getElementById(tagKg).innerHTML = kg;
 }
 
 function getKgFromPounds(pounds) {
-  const POUND_AS_KG = 0.4535924;
-  return pounds * POUND_AS_KG;
+  const poundAsKg = 0.4535924;
+  return pounds * poundAsKg;
 }
 
 function getNumberRoundDecimals(number, decimals) {
-  const TEN_RAISED = 10 ** decimals;
-  return Math.round(number * TEN_RAISED) / TEN_RAISED;
+  const tenRaised = 10 ** decimals;
+  return Math.round(number * tenRaised) / tenRaised;
 }
