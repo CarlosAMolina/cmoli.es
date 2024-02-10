3c8f32a28481d2c2299b34864ef0da003227606d
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-02-10 18:32:59 +0100

    Refactor move lines

--- deploy/md-to-html/src/main.py
+++ deploy/md-to-html/src/main.py
@@ -6,6 +6,19 @@ import sys
 import typing as tp
 
 
+def get_argument_parser():
+    parser = argparse.ArgumentParser(
+        description="Create script to convert .md files to .html"
+    )
+    parser.add_argument("css_pathname", type=str)
+    parser.add_argument("pandoc_metadata_file_pathname", type=str)
+    parser.add_argument("pandoc_script_convert_md_to_html_file_pathname", type=str)
+    parser.add_argument("pandoc_template_file_pathname", type=str)
+    parser.add_argument("pathname_to_analyze", type=str)
+    parser.add_argument("script_to_create_pathname", type=str)
+    return parser
+
+
 class Logger:
     @property
     def logger(self):
@@ -48,91 +61,43 @@ class Logger:
 logger = Logger().logger
 
 
-class DirectoryAnalyzer:
-    def get_md_pathnames(self, pathname: str) -> tp.Iterator[str]:
-        logger.debug(f"Init checking {pathname}")
-        for (dir_pathname, dirnames, filenames) in os.walk(
-            pathname, onerror=self._os_walk_exception_handler
-        ):
-            # print(dir_pathname, dirnames, filenames)
-            for filename in filenames:
-                if self._is_md_file(filename):
-                    yield str(pathlib.PurePath(dir_pathname, filename))
-
-    def _os_walk_exception_handler(self, exception_instance):
-        raise exception_instance
-
-    @staticmethod
-    def _is_md_file(filename: str) -> bool:
-        return pathlib.PurePath(filename).suffix.lower().strip() == ".md"
-
-
-class CssPathDetector:
-    def __init__(self, css_path: pathlib.PurePath):
-        self._css_path = css_path
-
-    def get_css_relative_pathname_from_file_path(
-        self, file_path: pathlib.PurePath
-    ) -> str:
-        css_pathname_without_filename = self._css_path.parent
-        file_pathname_without_filename = file_path.parent
-        return (
-            self._css_filename
-            if css_pathname_without_filename == file_pathname_without_filename
-            else self._get_css_relative_pathname_when_files_with_different_paths(
-                css_pathname_without_filename,
-                file_pathname_without_filename,
-            )
-        )
-
-    def _get_css_relative_pathname_when_files_with_different_paths(
-        self,
-        css_pathname_without_filename: pathlib.PurePath,
-        file_pathname_without_filename: pathlib.PurePath,
-    ) -> str:
-        folders_between_files_path: pathlib.PurePath = (
-            file_pathname_without_filename.relative_to(css_pathname_without_filename)
-        )
-        folders_between_files = str(folders_between_files_path).split("/")
-        relative_pathnames = [".." for _ in folders_between_files]
-        relative_pathname = "/".join(relative_pathnames)
-        result = "{}/{}".format(relative_pathname, self._css_filename)
-        return result
-
-    @property
-    def _css_filename(self) -> str:
-        return self._css_path.name
-
-
-def get_argument_parser():
-    parser = argparse.ArgumentParser(
-        description="Create script to convert .md files to .html"
-    )
-    parser.add_argument("css_pathname", type=str)
-    parser.add_argument("pandoc_metadata_file_pathname", type=str)
-    parser.add_argument("pandoc_script_convert_md_to_html_file_pathname", type=str)
-    parser.add_argument("pandoc_template_file_pathname", type=str)
-    parser.add_argument("pathname_to_analyze", type=str)
-    parser.add_argument("script_to_create_pathname", type=str)
-    return parser
-
-
-def get_path_substract_common_parts(
-    path_1: pathlib.PurePath, path_2: pathlib.PurePath
-) -> pathlib.PurePath:
-    return path_1.relative_to(path_2)
-
-
-def export_to_file_the_md_pathnames_to_convert(
+def run(
+    css_pathname: str,
+    pandoc_metadata_file_pathname: str,
+    pandoc_script_convert_md_to_html_file_pathname: str,
+    pandoc_template_file_pathname: str,
     pathname_to_analyze: str,
-    result_file_pathname: str,
+    script_to_create_pathname: str,
 ):
-    logger.debug(f"Init export file {result_file_pathname}")
-    with open(result_file_pathname, "w") as f:
-        for md_pathname in DirectoryAnalyzer().get_md_pathnames(pathname_to_analyze):
-            logger.debug(f"Detected .md file: {md_pathname}")
-            f.write(md_pathname)
-            f.write("\n")
+    logger.debug(f"Init export file {script_to_create_pathname}")
+    # TODO move constants to config.py
+    md_pathnames_to_convert_file_pathname = "/tmp/path-names-to-convert.txt"
+    md_pathnames_converted_file_pathname = "/tmp/path-names-converted.txt"
+    css_relative_pathnames_file_pathname = "/tmp/css-relative-pathnames.txt"
+    output_directory_pathname = pathname_to_analyze
+    export_to_file_the_md_pathnames_to_convert(
+        pathname_to_analyze, md_pathnames_to_convert_file_pathname
+    )
+    export_to_file_the_html_pathnames_converted(
+        pathname_to_analyze,
+        md_pathnames_to_convert_file_pathname,
+        output_directory_pathname=output_directory_pathname,
+        result_file_pathname=md_pathnames_converted_file_pathname,
+    )
+    export_to_file_the_css_relative_pathnames(
+        css_pathname,
+        md_pathnames_to_convert_file_pathname,
+        result_file_pathname=css_relative_pathnames_file_pathname,
+    )
+    export_to_file_the_script_combine_files(
+        pandoc_metadata_file_pathname,
+        pandoc_script_convert_md_to_html_file_pathname,
+        pandoc_template_file_pathname,
+        md_pathnames_to_convert_file_pathname,
+        md_pathnames_converted_file_pathname,
+        css_relative_pathnames_file_pathname,
+        script_to_create_pathname,
+    )
 
 
 def export_to_file_the_html_pathnames_converted(
@@ -155,31 +120,6 @@ def export_to_file_the_html_pathnames_converted(
             logger.debug(f"Pathname converted: {pathname_converted}")
             f_to_write.write(pathname_converted)
             f_to_write.write("\n")
-
-
-def get_pathname_converted(
-    analized_directory_pathname: str,
-    file_to_convert_pathname: str,
-    output_directory_pathname: str,
-) -> str:
-    path_to_convert = pathlib.PurePath(file_to_convert_pathname)
-    path_to_convert_without_analized_path = get_path_substract_common_parts(
-        path_to_convert, pathlib.PurePath(analized_directory_pathname)
-    )
-    filename_to_convert = path_to_convert.name
-    filename_converted = get_filename_set_extension(filename_to_convert, ".html")
-    path_converted_without_analized_path = (
-        path_to_convert_without_analized_path.with_name(filename_converted)
-    )
-    path_converted = pathlib.PurePath(output_directory_pathname).joinpath(
-        path_converted_without_analized_path,
-    )
-    pathname_converted = str(path_converted)
-    return pathname_converted
-
-
-def get_filename_set_extension(filename: str, extension: str) -> str:
-    return pathlib.PurePath(filename).with_suffix(extension).name
 
 
 def export_to_file_the_css_relative_pathnames(
@@ -240,45 +180,103 @@ def export_to_file_the_script_combine_files(
             script_file.write("\n")
 
 
-def run(
-    css_pathname: str,
-    pandoc_metadata_file_pathname: str,
-    pandoc_script_convert_md_to_html_file_pathname: str,
-    pandoc_template_file_pathname: str,
+class DirectoryAnalyzer:
+    def get_md_pathnames(self, pathname: str) -> tp.Iterator[str]:
+        logger.debug(f"Init checking {pathname}")
+        for (dir_pathname, dirnames, filenames) in os.walk(
+            pathname, onerror=self._os_walk_exception_handler
+        ):
+            # print(dir_pathname, dirnames, filenames)
+            for filename in filenames:
+                if self._is_md_file(filename):
+                    yield str(pathlib.PurePath(dir_pathname, filename))
+
+    def _os_walk_exception_handler(self, exception_instance):
+        raise exception_instance
+
+    @staticmethod
+    def _is_md_file(filename: str) -> bool:
+        return pathlib.PurePath(filename).suffix.lower().strip() == ".md"
+
+
+class CssPathDetector:
+    def __init__(self, css_path: pathlib.PurePath):
+        self._css_path = css_path
+
+    def get_css_relative_pathname_from_file_path(
+        self, file_path: pathlib.PurePath
+    ) -> str:
+        css_pathname_without_filename = self._css_path.parent
+        file_pathname_without_filename = file_path.parent
+        return (
+            self._css_filename
+            if css_pathname_without_filename == file_pathname_without_filename
+            else self._get_css_relative_pathname_when_files_with_different_paths(
+                css_pathname_without_filename,
+                file_pathname_without_filename,
+            )
+        )
+
+    def _get_css_relative_pathname_when_files_with_different_paths(
+        self,
+        css_pathname_without_filename: pathlib.PurePath,
+        file_pathname_without_filename: pathlib.PurePath,
+    ) -> str:
+        folders_between_files_path: pathlib.PurePath = (
+            file_pathname_without_filename.relative_to(css_pathname_without_filename)
+        )
+        folders_between_files = str(folders_between_files_path).split("/")
+        relative_pathnames = [".." for _ in folders_between_files]
+        relative_pathname = "/".join(relative_pathnames)
+        result = "{}/{}".format(relative_pathname, self._css_filename)
+        return result
+
+    @property
+    def _css_filename(self) -> str:
+        return self._css_path.name
+
+
+def get_path_substract_common_parts(
+    path_1: pathlib.PurePath, path_2: pathlib.PurePath
+) -> pathlib.PurePath:
+    return path_1.relative_to(path_2)
+
+
+def export_to_file_the_md_pathnames_to_convert(
     pathname_to_analyze: str,
-    script_to_create_pathname: str,
+    result_file_pathname: str,
 ):
-    logger.debug(f"Init export file {script_to_create_pathname}")
+    logger.debug(f"Init export file {result_file_pathname}")
+    with open(result_file_pathname, "w") as f:
+        for md_pathname in DirectoryAnalyzer().get_md_pathnames(pathname_to_analyze):
+            logger.debug(f"Detected .md file: {md_pathname}")
+            f.write(md_pathname)
+            f.write("\n")
 
-    # TODO move constants to config.py
-    md_pathnames_to_convert_file_pathname = "/tmp/path-names-to-convert.txt"
-    md_pathnames_converted_file_pathname = "/tmp/path-names-converted.txt"
-    css_relative_pathnames_file_pathname = "/tmp/css-relative-pathnames.txt"
-    output_directory_pathname = pathname_to_analyze
 
-    export_to_file_the_md_pathnames_to_convert(
-        pathname_to_analyze, md_pathnames_to_convert_file_pathname
+def get_pathname_converted(
+    analized_directory_pathname: str,
+    file_to_convert_pathname: str,
+    output_directory_pathname: str,
+) -> str:
+    path_to_convert = pathlib.PurePath(file_to_convert_pathname)
+    path_to_convert_without_analized_path = get_path_substract_common_parts(
+        path_to_convert, pathlib.PurePath(analized_directory_pathname)
     )
-    export_to_file_the_html_pathnames_converted(
-        pathname_to_analyze,
-        md_pathnames_to_convert_file_pathname,
-        output_directory_pathname=output_directory_pathname,
-        result_file_pathname=md_pathnames_converted_file_pathname,
+    filename_to_convert = path_to_convert.name
+    filename_converted = get_filename_set_extension(filename_to_convert, ".html")
+    path_converted_without_analized_path = (
+        path_to_convert_without_analized_path.with_name(filename_converted)
     )
-    export_to_file_the_css_relative_pathnames(
-        css_pathname,
-        md_pathnames_to_convert_file_pathname,
-        result_file_pathname=css_relative_pathnames_file_pathname,
+    path_converted = pathlib.PurePath(output_directory_pathname).joinpath(
+        path_converted_without_analized_path,
     )
-    export_to_file_the_script_combine_files(
-        pandoc_metadata_file_pathname,
-        pandoc_script_convert_md_to_html_file_pathname,
-        pandoc_template_file_pathname,
-        md_pathnames_to_convert_file_pathname,
-        md_pathnames_converted_file_pathname,
-        css_relative_pathnames_file_pathname,
-        script_to_create_pathname,
-    )
+    pathname_converted = str(path_converted)
+    return pathname_converted
+
+
+def get_filename_set_extension(filename: str, extension: str) -> str:
+    return pathlib.PurePath(filename).with_suffix(extension).name
 
 
 if __name__ == "__main__":
